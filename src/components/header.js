412b0b345d0f6f3aa3b92773a1503f9354a18d0e
Sunnyskp <skp2june@gmail.com>
2019-08-13 23:54:41 -0400

    Portfolio version 1 finalized

--- src/components/header.js
+++ src/components/header.js
@@ -5,43 +5,61 @@ import "../css/header-style.css"
 
 const Header = () => {
   return (
-    <Navbar.Collapse class="navbar-expand-lg">
+    <Navbar.Collapse>
       <Nav
         id="responsive-navbar-nav"
         className="justify-content-between"
         activeKey="/home"
       >
-        <Nav.Item className="nav-menu">
+        <Nav.Item
+          border="primary"
+          className="nav-menu btn btn-sm btn-outline-light btn-dark"
+        >
           <Nav.Link className="nav-menu-link" href="/">
             Home
           </Nav.Link>
         </Nav.Item>
-        <Nav.Item className="nav-menu">
+        <Nav.Item
+          border="primary"
+          className="nav-menu btn btn-sm btn-outline-light btn-dark"
+        >
           <Nav.Link className="nav-menu-link" href="/portfolio">
             Portfolio
           </Nav.Link>
         </Nav.Item>
-        <Nav.Item className="nav-menu">
+        <Nav.Item
+          border="primary"
+          className="nav-menu btn btn-sm btn-outline-light btn-dark"
+        >
           <Nav.Link className="nav-menu-link" href="/services">
             Services
           </Nav.Link>
         </Nav.Item>
-        <Nav.Item className="nav-menu">
+        <Nav.Item className="nav-menu ">
           <Nav.Link className="nav-menu-link" href="/">
             <img src={Logo} className="logo-style" alt="" />
           </Nav.Link>
         </Nav.Item>
-        <Nav.Item className="nav-menu">
+        <Nav.Item
+          border="primary"
+          className="nav-menu btn btn-sm btn-outline-light btn-dark"
+        >
           <Nav.Link className="nav-menu-link" href="/about">
             About Me
           </Nav.Link>
         </Nav.Item>
-        <Nav.Item className="nav-menu">
+        <Nav.Item
+          border="primary"
+          className="nav-menu btn btn-sm btn-outline-light btn-dark"
+        >
           <Nav.Link className="nav-menu-link" href="/contact">
             Contact Me
           </Nav.Link>
         </Nav.Item>
-        <Nav.Item className="nav-menu">
+        <Nav.Item
+          border="primary"
+          className="nav-menu btn btn-sm btn-outline-light btn-dark"
+        >
           <Nav.Link
             className="nav-menu-link"
             href="https://github.com/Sunnyskp"
