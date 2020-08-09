27b8dc60ffb6ae722f4a00882e1e927f4afabce7
Sunny Krishnan Pillai <skp2june@gmail.com>
2020-08-09 19:20:19 -0400

    Old Portfolio Update

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,12 +2,12 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("C:\\Humber_Sunny\\Portfolio-Using-Gatsby-Bootstrap\\Portfolio-Using-Gatsby-Bootstrap\\.cache\\dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("C:\\Humber_Sunny\\Portfolio-Using-Gatsby-Bootstrap\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-about-js": () => import("C:\\Humber_Sunny\\Portfolio-Using-Gatsby-Bootstrap\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\about.js" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-contact-js": () => import("C:\\Humber_Sunny\\Portfolio-Using-Gatsby-Bootstrap\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
-  "component---src-pages-index-js": () => import("C:\\Humber_Sunny\\Portfolio-Using-Gatsby-Bootstrap\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-portfolio-js": () => import("C:\\Humber_Sunny\\Portfolio-Using-Gatsby-Bootstrap\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\portfolio.js" /* webpackChunkName: "component---src-pages-portfolio-js" */),
-  "component---src-pages-services-js": () => import("C:\\Humber_Sunny\\Portfolio-Using-Gatsby-Bootstrap\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\services.js" /* webpackChunkName: "component---src-pages-services-js" */)
+  "component---cache-dev-404-page-js": () => import("C:\\WebDev\\Portfolio-Using-Gatsby-Bootstrap\\.cache\\dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("C:\\WebDev\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-js": () => import("C:\\WebDev\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-contact-js": () => import("C:\\WebDev\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-index-js": () => import("C:\\WebDev\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-portfolio-js": () => import("C:\\WebDev\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\portfolio.js" /* webpackChunkName: "component---src-pages-portfolio-js" */),
+  "component---src-pages-services-js": () => import("C:\\WebDev\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\services.js" /* webpackChunkName: "component---src-pages-services-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,12 +5,12 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Humber_Sunny\\Portfolio-Using-Gatsby-Bootstrap\\Portfolio-Using-Gatsby-Bootstrap\\.cache\\dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("C:\\Humber_Sunny\\Portfolio-Using-Gatsby-Bootstrap\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\404.js"))),
-  "component---src-pages-about-js": hot(preferDefault(require("C:\\Humber_Sunny\\Portfolio-Using-Gatsby-Bootstrap\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\about.js"))),
-  "component---src-pages-contact-js": hot(preferDefault(require("C:\\Humber_Sunny\\Portfolio-Using-Gatsby-Bootstrap\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\contact.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("C:\\Humber_Sunny\\Portfolio-Using-Gatsby-Bootstrap\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\index.js"))),
-  "component---src-pages-portfolio-js": hot(preferDefault(require("C:\\Humber_Sunny\\Portfolio-Using-Gatsby-Bootstrap\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\portfolio.js"))),
-  "component---src-pages-services-js": hot(preferDefault(require("C:\\Humber_Sunny\\Portfolio-Using-Gatsby-Bootstrap\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\services.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\WebDev\\Portfolio-Using-Gatsby-Bootstrap\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\WebDev\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("C:\\WebDev\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\about.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("C:\\WebDev\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\contact.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\WebDev\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\index.js"))),
+  "component---src-pages-portfolio-js": hot(preferDefault(require("C:\\WebDev\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\portfolio.js"))),
+  "component---src-pages-services-js": hot(preferDefault(require("C:\\WebDev\\Portfolio-Using-Gatsby-Bootstrap\\src\\pages\\services.js")))
 }
 
